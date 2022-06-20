2265c963fa3240d8149de21f97e73b1e184d402f
kevinCaldieraro <kevincaldieraro123@gmail.com>
2022-06-20 16:30:46 -0300

    update exercise

--- http_pageServer/index.js
+++ http_pageServer/index.js
@@ -2,26 +2,30 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+function handleFile(req, res) {
+  let path = url.parse(req.url).pathname;
+
+  if (path == '' || path == '/') {
+    path = '/index.html';
+  }
+
+  let fileName = '.' + path;
+
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html;charset=utf-8' });
+      res.end('<h1>Page not found</h1>');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    }
+  });
+}
+
 http
   .createServer((request, response) => {
-    let path = url.parse(request.url).pathname;
-
-    if (path == '' || path == '/') {
-      path = '/index.html';
-    }
-
-    let fileName = '.' + path;
-
-    fs.readFile(fileName, (err, data) => {
-      if (err) {
-        response.writeHead(404, { 'Content-Type': 'text/html;charset=utf-8' });
-        response.end('<h1>Page not found</h1>');
-      } else {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(data);
-        response.end();
-      }
-    });
+    handleFile(request, response);
   })
   .listen(3000, error => {
     if (error) {
